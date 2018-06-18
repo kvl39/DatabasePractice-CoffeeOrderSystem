065b619a3987d49972b6e1656bc0d6f3bab37d3a
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-06-18 10:27:54 +0800

    Add funtion: setup StackItem View

---
+++ AWSCoffee/AWSCoffee/View/StackItemView/StackItemView.swift
@@ -0,0 +1,15 @@
+//
+//  StackItemView.swift
+//  AWSCoffee
+//
+//  Created by Liao Kevin on 2018/6/18.
+//  Copyright © 2018年 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class StackItemView: UIView {
+
+    
+
+}
